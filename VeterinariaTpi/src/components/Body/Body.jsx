19a8f7cf264e93867424f43ced6ff5659e2680ee
unknown <tomasburack@gmail.com>
2025-09-03 19:28:03 -0300

    pre enrutado

---
+++ VeterinariaTpi/src/components/Body/Body.jsx
@@ -0,0 +1,13 @@
+import BackgroundCarousel from "../BackgroundCarousel/BackgroundCarousel"
+import Services from "../CServices/Services"
+
+const Body = () => {
+  return (
+    <div>
+        <BackgroundCarousel/>
+        <Services/>
+    </div>
+  )
+}
+
+export default Body
---
+++ VeterinariaTpi/src/components/Login/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Button, Form, Card, Row, Col } from "react-bootstrap";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailInput = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordInput = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <Card className="w-25">
+        <Card.Body>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group className="mb-5">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Ingresar Mail"
+                    onChange={handleEmailInput}
+                    value={email}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-5">
+                  <Form.Label>Contraseña</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Ingresar Contraseña"
+                    onChange={handlePasswordInput}
+                    value={password}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-flex flex-column justify-content-end align-items-end">
+                <Button variant="primary" type="submit">
+                  Iniciar Sesion
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
