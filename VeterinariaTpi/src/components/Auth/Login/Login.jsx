700d9d73b90c089f6d8c4874680f9cd1ac0c54fd
Cristian Salinas <salinas.cristian15@gmail.com>
2025-09-14 19:21:25 -0300

    added login/register functionality

--- VeterinariaTpi/src/components/Auth/Login/Login.jsx
+++ VeterinariaTpi/src/components/Auth/Login/Login.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import { Button, Form, Card, Row, Col, NavLink } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import "../Login/Login.css";
+import { errorToast } from "../../shared/notifications/notifications";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
 
   const handleEmailInput = (event) => {
     setEmail(event.target.value);
@@ -14,11 +18,37 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch("http://localhost:3000/login", {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+
+      .then(res => res.json().then(data => {
+        if (!res.ok) {
+          errorToast(data.message)
+          return;
+        }
+
+        localStorage.setItem("vetCare-token", data)
+        navigate('/userpanel')
+      }))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <Card className="w-25">
         <Card.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col>
                 <Form.Group className="mb-5">
