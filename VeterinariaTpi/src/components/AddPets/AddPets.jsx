ea0828b0471049476492cc5268f673532ea7c1da
Agustin Sentis <agussentis@gmail.com>
2025-09-20 18:34:33 -0300

    added validations for AddPets form

--- VeterinariaTpi/src/components/AddPets/AddPets.jsx
+++ VeterinariaTpi/src/components/AddPets/AddPets.jsx
@@ -1,36 +1,85 @@
 import { useState } from "react"
 import { Button, Card, Col, Row, Form } from "react-bootstrap"
 import { useNavigate } from "react-router";
+import {validateAddPetName, validatePetAge, validateBreed} from "../shared/validations.js"
+import { errorToast } from "../shared/notifications/notifications.js";
 
 export const AddPets = () => {
     const [petName, setPetName] = useState("");
     const [petAge, setPetAge] = useState("");
     const [petBreed, setPetBreed] = useState("");
+    const [errors, setErrors] = useState({});
 
     const navigate = useNavigate();
 
     const handleNameInput = (e) => {
-        setPetName(e.target.value);
+        const value = e.target.value;
+        setPetName(value);
+        setErrors({ ...errors,
+            petName: validateAddPetName(value)
+        });
     } 
 
     const handleAgeInput = (e) => {
-        setPetAge(e.target.value);
+        const value = e.target.value;
+        setPetAge(value);
+        setErrors({ ...errors,
+            petAge: validatePetAge(value)
+        });
     }
 
     const handleBreedInput = (e) => {
-        setPetBreed(e.target.value);
+        const value = e.target.value;
+        setPetBreed(value);
+        setErrors({ ...errors,
+            petBreed: validateBreed(value)
+        });
     }
 
     const handleBackClick = () => {
         navigate("/userpanel");
     }
 
+    const handleSubmit = (e) => {
+            e.preventDefault()
+    
+            const formErrors = {
+                petName: validateAddPetName(petName),
+                petAge: validatePetAge(petAge),
+                petBreed: validateBreed(petBreed)
+            };
+    
+            setErrors(formErrors);
+    
+            const hasErrors = Object.values(formErrors).some(err => err !== "");
+    
+            if(hasErrors){
+                errorToast("Hay algunos campos incorrectos, revisalos.")
+                return;
+            }
+    
+            fetch('http://localhost:3000/addpet', {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    petName,
+                    petAge,
+                    petBreed
+                })
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+        }
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-        <Card>
+        <Card className="mt-1 mb-1">
             <Card.Body>
                 <h2>Añade a tu mascota</h2>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Row>
                         <Col>
                         <Form.Group className="mb-5">
@@ -40,7 +89,11 @@ export const AddPets = () => {
                                 placeholder="Ingrese el nombre"
                                 onChange={handleNameInput}
                                 value={petName}
+                                isInvalid={errors.petName}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.petName}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group className="mb-5">
                             <Form.Label>Edad:</Form.Label>
@@ -49,7 +102,11 @@ export const AddPets = () => {
                                 placeholder="Ingrese su edad"
                                 onChange={handleAgeInput}
                                 value={petAge}
+                                isInvalid={errors.petAge}
                             />
+                            <Form.Control.Feedback type="invalid" style={{ whiteSpace: "pre-line" }}>
+                                {errors.petAge}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Raza:</Form.Label>
@@ -58,7 +115,11 @@ export const AddPets = () => {
                                 placeholder="Ingrese su raza:"
                                 onChange={handleBreedInput}
                                 value={petBreed}
+                                isInvalid={errors.petBreed}
                             />
+                            <Form.Control.Feedback type="invalid" style={{ whiteSpace: "pre-line" }}>
+                                {errors.petBreed}
+                            </Form.Control.Feedback>
                         </Form.Group>
                         </Col>
                     </Row>
--- VeterinariaTpi/src/components/shared/validations.js
+++ VeterinariaTpi/src/components/shared/validations.js
@@ -3,6 +3,9 @@ const regexLastName = /^[a-zA-ZÀ-ÿ\s]+$/;
 const regexDni = /^\d{7,8}$/;
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const regexPassword = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{7,20}$/;
+const regexPetName = /^[a-zA-ZÀ-ÿ]+$/;
+const regexAge = /^[1-9]\d?$/
+const regexBreed = /^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{2,20}$/
 
 export const validateFirstName = (value) => {
     if(!regexFirstName.test(value))
@@ -32,4 +35,28 @@ export const validatePassword = (value) => {
     if(!regexPassword.test(value))
         return "La contraseña debe tener:\n entre 7 y 20 caracteres\n al menos una mayúscula y un número";
     return "";
+}
+
+export const validateAddPetName = (value) => {
+    if(!value)
+        return "El nombre debe ser obligatorio."
+    if(!regexPetName.test(value))
+        return "El nombre solo puede contener letras"
+    return ""
+}
+
+export const validatePetAge = (value) => {
+    if(!value)
+        return "La edad es obligatoria."
+    if(!regexAge.test(value))
+        return "La edad no puede ser 0 o menos.\n Debe contener como maximo 2 caracteres."
+    return ""
+}
+
+export const validateBreed = (value) => {
+    if(!value)
+        return "La raza es obligatoria."
+    if(!regexBreed.test(value))
+        return "Solo puede contener letras.\n Debe contener entre 2 y 20 caracteres"
+    return ""
 }
