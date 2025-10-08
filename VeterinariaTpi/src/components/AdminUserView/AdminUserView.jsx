878160185298c437ee5a72a1a1022765663b19c2
tomas burack <tomasburack@gmail.com>
2025-10-08 01:19:40 -0300

    connection to the backend completed

--- VeterinariaTpi/src/components/AdminUserView/AdminUserView.jsx
+++ VeterinariaTpi/src/components/AdminUserView/AdminUserView.jsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { useAuth } from "../../Services/authContext/AuthContext";
 
 const AdminUserView = () => {
-    const [users, setUsers] = useState()
+  const [users, setUsers] = useState();
+  const { token } = useAuth();
 
-    useEffect(()=>{
-        fetch("http://localhost:3000/users")
-            .then(res => res.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    },[])
-  return (
-    <div className="w-100 h-100"></div>
-  )
-}
+  useEffect(() => {
+    fetch("http://localhost:3000/users", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-export default AdminUserView
+  return <div className="w-100 h-100"></div>;
+};
+
+export default AdminUserView;
