8edbe1348c9c76ce6de7eeeb7202b627c2fdd613
Aldana Iramaz <aldanairamaz@hotmail.com>
2025-10-18 15:54:48 -0300

    PanelHistoryShift

---
+++ VeterinariaTpi/src/components/ShiftHistory/ShiftHistory.jsx
@@ -0,0 +1,19 @@
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router";
+
+
+const ShiftHistory = () => {
+
+  return (
+    <>
+        <h1>Historial de turnos: </h1>
+    
+    </>
+  );
+};
+
+export default ShiftHistory;
